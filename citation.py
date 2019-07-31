ff0635c83f10da3b2942f49986742940ce3610b9
liu-jc <jc_liu@qq.com>
2019-07-31 16:30:09 +0800

    updat result

--- citation.py
+++ citation.py
@@ -118,7 +118,8 @@ def save_time_result(file_name, *args):
         pickle.dump(save_dict, f)
 
 print("Validation Accuracy: {:.4f} Test Accuracy: {:.4f}".format(acc_val, acc_test))
-print("Pre-compute time: {:.4f}s, train time: {:.4f}s, total: {:.4f}s".format(precompute_time, train_time, precompute_time+train_time))
+print("Pre-compute time: {:.4f}s, train time: {:.4f}s, total: {:.4f}s".format(precompute_time, train_time,
+                                                                              precompute_time+train_time))
 
 print("------Training time Details-------")
 print("Total Training Time: {:.4f}s".format(train_time))
@@ -135,5 +136,5 @@ print_time_ratio('Backward Time', backward_time, train_time)
 print_time_ratio('Step Time', step_time, train_time)
 
 file_name = os.path.join('time_result', args.dataset)
-save_time_result(file_name, 'precompute_time', 'train_time', 'forward_time', 'cross_entropy_time', 'softmax_time', 'nll_time',
-                 'backward_time', 'step_time')
+save_time_result(file_name, 'precompute_time', 'train_time', 'forward_time', 'cross_entropy_time', 'softmax_time',
+                 'nll_time','backward_time', 'step_time')
